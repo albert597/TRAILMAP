dcd4f201e1f97faa3dce6e0630a81d9387f96621
Albert Pun <albertpun8@gmail.com>
2020-04-04 22:02:26 -0700

    Updated label_processor.py to work when axons=1 artifacts=2

--- training/label_processor.py
+++ training/label_processor.py
@@ -1,20 +1,19 @@
 import numpy as np
 
+# 1 should be axons and 2 should be artifacts
 def process_labels(vol):
 
     for i in range(vol.shape[0]):
         slice = vol[i]
 
         if np.max(slice) > 0:
+            # Increment whole slice by 1 to have background be 1
+            slice += 1
             for x in range(1, slice.shape[0] - 1):
                 for y in range(1, slice.shape[1] - 1):
                     if slice[x][y] == 1 and is_axon_close(slice, x, y):
                         # Set edge label
-                        # Note: Edge label is set to 3 because the whole slice is incremented by 1 in the end
-                        slice[x][y] = 3
-
-        # Increment whole slice by 1 to have background be 1
-        slice += 1
+                        slice[x][y] = 4
 
     return vol
 
